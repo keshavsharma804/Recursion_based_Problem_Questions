dd3bc0ff27a39ddc9ba40d3ca36cda05411b9e16
Keshav Sharma <keshavsharma2868@gmail.com>
2024-11-07 17:34:47 -0700

    Update Recursion.py

--- Recursion.py
+++ Recursion.py
@@ -17,3 +17,35 @@
 
 # n = int(input("Enter a number: "))
 # print("Sum of first", n, "natural numbers:", sum_of_natural_numbers(n))
+
+
+# def fibonacci(n):
+#     if n == 0:
+#         return 0
+#     elif n == 1:
+#         return 1
+#     return fibonacci(n - 1) + fibonacci(n - 2)
+
+# n = int(input("Enter a number: "))
+# print(f"Fibonacci({n}) =", fibonacci(n))
+
+
+# ## Print numbers 1 to n
+# def num(n):
+#     if n<= 1:
+#         return 
+#     num(n-1)
+#     print(n)
+# n = int(input("ENter the number"))
+# num(n)
+
+
+
+# ## print numbers n to 1
+# def num(n):
+#     if n<= 1:
+#         return
+#     print(n) 
+#     num(n-1)   
+# n = int(input("ENter the number"))
+# num(n)
